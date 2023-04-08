f030c81f4fe560c35779592c98a26caa1fb788e6
iuliailies <72351491+iuliailies@users.noreply.github.com>
2023-04-08 15:44:31 +0300

    feat(zoom-environment): zoom options

--- src/zoom/zoom-environment.ts
+++ src/zoom/zoom-environment.ts
@@ -1,35 +1,93 @@
 import { Point } from "../drag/drag-environment";
 import { SketcherHTMLElement } from "../models/sketcher-html-element";
 import { select } from "../selection/select";
+import { isShortcutPressed } from "../showcase/helpers";
 
 export type ZoomState = "zoom";
+
+export interface ZoomOptions {
+  // how much the zoom value should be incremented/decremented inside one callback;
+  step?: number;
+  // maximum zoom value
+  upperBound?: number;
+  // minimum zoom value
+  lowerBound?: number;
+  // method for triggering zoom behaviour;
+  method?: {
+    type: "mouse" | "keyboard";
+    ctrlKey?: boolean;
+  };
+}
 
 export class ZoomEnvironment {
   outsideFunctionBindings = new Map<
     ZoomState,
     (this: SketcherHTMLElement, eventObj: Event, zoom: number) => any
   >();
+
+  defaultOptions: ZoomOptions = {
+    step: 0.1,
+    method: {
+      type: "mouse",
+      ctrlKey: true,
+    },
+  };
   constructor() {}
 
   public apply(
     zoomableContainer: HTMLElement,
     zoomable: HTMLElement,
-    zoom: number = 1
+    options: ZoomOptions = this.defaultOptions
   ): ZoomEnvironment {
-    zoomableContainer.addEventListener("wheel", (event: WheelEvent) => {
-      const prevZoom = zoom;
-      zoom += event.deltaY > 0 ? -0.1 : 0.1;
-      const mousePosition = { x: event.pageX, y: event.pageY };
-      this.handleZoom(
-        zoomableContainer,
-        zoomable,
-        zoom,
-        prevZoom,
-        mousePosition
-      );
-      // call user defined function
-      this.outsideFunctionBindings.get("zoom")?.apply(zoomable, [event, zoom]);
-    });
+    let zoom = +(
+      zoomable.getBoundingClientRect().width / zoomable.offsetWidth
+    ).toFixed(1);
+
+    const method = options.method ? options.method.type : "mouse";
+
+    if (method === "mouse") {
+      zoomableContainer.addEventListener("wheel", (event: WheelEvent) => {
+        if (
+          !options.method ||
+          (options.method && options.method.ctrlKey === event.ctrlKey)
+        ) {
+          const direction = event.deltaY < 0 ? -1 : 1;
+          const mousePosition = { x: event.pageX, y: event.pageY };
+          zoom = this.zoomCallback(
+            zoomableContainer,
+            zoomable,
+            zoom,
+            direction,
+            options,
+            event,
+            mousePosition
+          );
+        }
+      });
+    } else {
+      document.addEventListener("keydown", (event: KeyboardEvent) => {
+        if (
+          !options.method ||
+          (options.method && options.method.ctrlKey === event.ctrlKey)
+        ) {
+          const direction = isShortcutPressed(event, { ctrl: true, key: "+" })
+            ? -1
+            : isShortcutPressed(event, { ctrl: true, key: "-" })
+            ? 1
+            : undefined;
+          if (direction) {
+            zoom = this.zoomCallback(
+              zoomableContainer,
+              zoomable,
+              zoom,
+              direction,
+              options,
+              event
+            );
+          }
+        }
+      });
+    }
     return this;
   }
 
@@ -41,31 +99,60 @@ export class ZoomEnvironment {
     return this;
   }
 
+  private zoomCallback(
+    zoomableContainer: HTMLElement,
+    zoomable: HTMLElement,
+    zoom: number,
+    direction: -1 | 1,
+    options: ZoomOptions,
+    event: Event,
+    mousePosition?: Point
+  ): number {
+    const step = options.step !== undefined ? options.step : 0.1;
+    const prevZoom = zoom;
+
+    zoom += direction > 0 ? -step : step;
+    if (
+      (options.upperBound && zoom >= options.upperBound) ||
+      (options.lowerBound && zoom <= options.lowerBound)
+    ) {
+      // call user defined function
+      this.outsideFunctionBindings.get("zoom")?.apply(zoomable, [event, zoom]);
+      return prevZoom;
+    }
+
+    this.handleZoom(zoomableContainer, zoomable, zoom, prevZoom, mousePosition);
+    // call user defined function
+    this.outsideFunctionBindings.get("zoom")?.apply(zoomable, [event, zoom]);
+
+    return zoom;
+  }
+
   private handleZoom(
     zoomableContainer: HTMLElement,
     zoomable: HTMLElement,
     zoom: number,
     prevZoom: number,
-    mousePosition: Point,
-    onCenter: boolean = false
+    mousePosition?: Point
   ): void {
     const zoomableContainerRect = zoomableContainer.getBoundingClientRect();
     const zoomableRect = zoomable.getBoundingClientRect();
 
     select(zoomable).style("transform-origin", "0 0");
 
+    // no mouse position specified => zoom towards the center
     const zoomCenter: Point = {
       x:
-        (onCenter
+        (!mousePosition
           ? zoomableContainerRect.left + zoomableContainerRect.width / 2
           : mousePosition.x) - document.documentElement.scrollLeft,
       y:
-        (onCenter
+        (!mousePosition
           ? zoomableContainerRect.top + zoomableContainerRect.height / 2
           : mousePosition.y) - document.documentElement.scrollTop,
     };
 
-    // avoid slight center deviation by approximating at 2 fixed decimals
+    // avoid slight center deviation by approximating with 2 fixed decimals
     let xPercent = +(
       (zoomCenter.x - zoomableRect.left) /
       zoomableRect.width
